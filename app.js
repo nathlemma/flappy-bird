0deba68be13c299ce1efa63b8c793ea76a40467c
Nathan <42325916+nathlemma@users.noreply.github.com>
2023-05-23 21:14:05 -0400

    save progress

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let numPlatform = 4
   let platforms = []
   const gravity = 1
+  let gap
   const platformSpeed = 4
   let time = 0 //seconds
   let flag = true
   let gameOver = false
   let movePlatformID
   let fallID
+  let jumpID
   let checkHeightID
   let eventListener
 
@@ -21,23 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
     cat.style.left = catLeftSpace + "px"
     cat.style.bottom = catBottomSpace + "px"
   }
-
-  function endGame(){
-    clearInterval(movePlatformID)
-    clearInterval(fallID)
-    clearInterval(checkHeightID)
-    gameOver = true
-  }
-  function freeFall() {
-    console.log("cat bottom: " + catBottomSpace)
-    if (catBottomSpace < 0) {
-      endGame()
-    }
-    catBottomSpace += gravity * (-2 * time + 10  )
-    cat.style.bottom = catBottomSpace + "px"
-    time += 1
-  }
-
   class Platform {
     constructor(bottom) {
       this.bottom = bottom
@@ -53,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function makePlatforms() {
     for (let i = 0; i < numPlatform; i++) {
-      let gap = 600 / numPlatform
+      gap = 600 / numPlatform
       let bottom = i * gap + 60
       platforms.push(new Platform(bottom))
       console.log(platforms)
@@ -61,17 +46,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function movePlatforms(pixel) {
-    if (!flag) {
-      if (catBottomSpace < 0) {
-        endGame()
-      }
-      catBottomSpace -= pixel
-      cat.style.bottom = catBottomSpace + "px"
-    }
     platforms.forEach((platform) => {
       platform.bottom -= pixel
       let element = platform.element
       element.style.bottom = platform.bottom + "px"
+      if (platform.bottom < 0) {
+        let removedPlat = platforms.shift()
+        removedPlat.element.classList.remove('platform')
+        platforms.push(new Platform(600))
+      }
     })
   }
 
@@ -91,69 +74,76 @@ document.addEventListener("DOMContentLoaded", () => {
     cat.style.left = catLeftSpace + "px"
   }
 
-  function checkHeight(){
-    function compare(div1, div2) {
-      var rect1 = div1.getBoundingClientRect()
-      var rect2 = div2.getBoundingClientRect()
-      if (
-        rect1.left < rect2.right &&
-        rect1.right > rect2.left &&
-        rect1.top < rect2.bottom &&
-        rect1.bottom > rect2.top
-      ) {
-        clearInterval(fallID)
-        flag = false
-        console.log("Divs are touching or overlapping.")
-        // Do something when the divs touch or overlap
-      } else {
-        // Do something when the divs do not touch
-      }
-    }
-    platforms.forEach((p) => {
-      compare(p.element, cat)
-    })
+  function jump(){
   }
+
+  function fall() {
+    isJumping = false
+      clearInterval(upTimerId)
+      downTimerId = setInterval(function () {
+        doodlerBottomSpace -= 5
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+        if (doodlerBottomSpace <= 0) {
+          gameOver()
+        }
+        
+        })
+  
+      },20)
+  }
+
+  // platforms.forEach(platform => {
+  //   if (
+  //     (doodlerBottomSpace >= platform.bottom) &&
+  //     (doodlerBottomSpace <= (platform.bottom + 15)) &&
+  //     ((doodlerLeftSpace + 60) >= platform.left) && 
+  //     (doodlerLeftSpace <= (platform.left + 85)) &&
+  //     !isJumping
+  //     ) {
+  //       console.log('tick')
+  //       startPoint = doodlerBottomSpace
+  //       jump()
+  //       console.log('start', startPoint)
+  //       isJumping = true
+  //     }
+  function freeFall() {
+    console.log("cat bottom: " + catBottomSpace)
+    if (catBottomSpace < 0) {
+      endGame()
+    }
+    catBottomSpace += gravity * (-2 * time + 10  )
+    cat.style.bottom = catBottomSpace + "px"
+    time += 1
+  }
+
+  // function checkHeight(){
+  //   function compare(div1, div2) {
+  //     var rect1 = div1.getBoundingClientRect()
+  //     var rect2 = div2.getBoundingClientRect()
+  //     if (
+  //       rect1.left < rect2.right &&
+  //       rect1.right > rect2.left &&
+  //       rect1.top < rect2.bottom &&
+  //       rect1.bottom > rect2.top
+  //     ) {
+  //       clearInterval(fallID)
+  //       flag = false
+  //       console.log("Divs are touching or overlapping.")
+  //       // Do something when the divs touch or overlap
+  //     } else {
+  //       // Do something when the divs do not touch
+  //     }
+  //   }
+  //   platforms.forEach((p) => {
+  //     compare(p.element, cat)
+  //   })
+  // }
   function run() {
     makeCat()
     makePlatforms()
-
-    movePlatformID = setInterval(movePlatforms, 50, platformSpeed)
-    fallID = setInterval(freeFall, 50)
-    checkHeightID = setInterval(checkHeight, 1)
-
-    eventListener = document.addEventListener("keydown", (event) => {
-      if (event.key == "ArrowUp" && !gameOver) {
-        moveUp(100)
-        time = 0
-        if (!flag) {
-          flag = true
-          fallID = setInterval(freeFall, 50)
-          checkHeightID = setInterval(checkHeight, 1)
-        }
-      }
-      if (event.key == "ArrowRight" && !gameOver) {
-        moveRight(30)
-        time = 0
-        if (!flag) {
-          flag = true
-          fallID = setInterval(freeFall, 50)
-          checkHeightID = setInterval(checkHeight, 1)
-        }
-      }
-
-      if (event.key == "ArrowLeft" && !gameOver) {
-        moveLeft(30)
-        time = 0
-        if (!flag) {
-          flag = true
-          fallID = setInterval(freeFall, 50)
-          checkHeightID = setInterval(checkHeight, 1)
-        }
-      } else {
-      }
-      console.log("pressed: " + event.key)
-    })
+    setInterval(movePlatforms,30,platformSpeed)
   }
+  
   
   run()
 })
